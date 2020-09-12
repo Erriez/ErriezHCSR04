48e3f6b4215bddb6bbbb08bcc74be28794cbe32e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-12 12:56:17 +0000

    Deploy Erriez/ErriezHCSR04 to github.com/Erriez/ErriezHCSR04.git:gh-pages

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getdistance',['getDistance',['../class_erriez_h_c_s_r04.html#a2219a874610f683d7139950c1380ef5a',1,'ErriezHCSR04']]]
+  ['getdistance',['getDistance',['../class_erriez_h_c_s_r04.html#a238bfff961c32490c5e8b16f5d9c8bfa',1,'ErriezHCSR04']]],
+  ['getdistanceinch',['getDistanceInch',['../class_erriez_h_c_s_r04.html#ad49c2ea839d54542acc41af3444c3c01',1,'ErriezHCSR04']]]
 ];
